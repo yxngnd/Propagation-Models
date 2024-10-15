31613577f29499c90c3670e6ea6b9bb21c40345b
Nirvan Yang <yangnyd@gmail.com>
2024-10-15 14:34:45 -0300

    updated free space

--- free_space.m
+++ free_space.m
@@ -1,6 +1,4 @@
 function L = free_space(pt, gt, gr, lambda, f, d, L)
-  % d: distância em km
-  % f: frequência em MHz
   A = (lambda^2 * gr) / 4 * pi;
   Pr = ((pt * gt) / 4 * pi * d.^2) * A
   L = 32.44 - 10 * log10(gt) - 10 * log10(gr) + 20 * log10(f) + 20 * log10(d);
