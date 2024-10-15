990ac2d01639417770c4addcc63995aa83c264c9
Nirvan Yang <yangnyd@gmail.com>
2024-10-15 15:06:55 -0300

    updated free space

--- free_space.m
+++ free_space.m
@@ -1,6 +1,6 @@
 function L = free_space(pt, gt, gr, lambda, f, d, L)
   A = (lambda^2 * gr) / 4 * pi;
-  Pr = ((pt * gt) / 4 * pi * d.^2) * A
+  Pr = ((pt * gt) / 4 * pi * d.^2) * A;
   L = 32.44 - 10 * log10(gt) - 10 * log10(gr) + 20 * log10(f) + 20 * log10(d);
 end
 
